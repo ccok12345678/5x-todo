1723bb446ca660d0f635124f2ff1ffdda9e54f19
Karl Yang <ccok12345678@gmail.com>
2022-04-02 14:59:45 +0800

    init

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: [
+    'plugin:vue/vue3-essential',
+    'standard'
+  ],
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module'
+  },
+  plugins: [
+    'vue'
+  ],
+  rules: {
+  }
+}
---
+++ src/views/HomeView.vue
@@ -0,0 +1,9 @@
+<template>
+  <HelloWorld :msg="msg"></HelloWorld>
+</template>
+
+<script setup>
+import HelloWorld from '@/components/HelloWorld.vue'
+
+const msg = 'Hello Rabbit'
+</script>
