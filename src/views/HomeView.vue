2a70eaab5aee2826d4af05a2403b8d90adf3bc71
Karl Yang <ccok12345678@gmail.com>
2022-04-02 21:05:21 +0800

    added HomeView illustration

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,23 @@
 <template>
-  <HelloWorld :msg="msg"></HelloWorld>
+  <div class="d-flex justify-content-center vh-100 bg-primary">
+
+    <div class="w-100 d-flex justify-content-end align-items-center">
+        <img
+          src="@/assets/images/left.svg"
+          alt="online todo list"
+          >
+    </div>
+
+    <div class="w-100 d-flex justify-content-center align-items-center">
+      right
+    </div>
+
+  </div>
 </template>
 
-<script setup>
-import HelloWorld from '@/components/HelloWorld.vue'
-
-const msg = 'Hello Rabbit'
-</script>
+<style lang="scss" scoped>
+img {
+  width: 83.333%;
+  max-width: 386px;
+}
+</style>
