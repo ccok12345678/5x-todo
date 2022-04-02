f72ba8b0e0c404fb1ff27acc06acb5bb44c2db7f
Karl Yang <ccok12345678@gmail.com>
2022-04-02 23:29:00 +0800

    added brand logo

---
+++ src/components/BrandLogo.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="logo">
+
+    <router-link
+      to="/"
+      class="text-dark d-flex align-items-center"
+      title="Online Todo List"
+      >
+
+      <img src="@/assets/images/logo.svg" alt="logo">
+
+      <h1 class="mb-0">ONLINE TODO LIST</h1>
+
+    </router-link>
+
+  </div>
+</template>
+
+<style lang="scss" scoped>
+a:hover {
+  text-decoration: none;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,22 +1,37 @@
 <template>
   <div class="d-flex justify-content-center vh-100 bg-primary">
 
-    <div class="w-100 d-flex justify-content-end align-items-center">
+    <div class="w-100 d-flex flex-column justify-content-center align-items-end">
+
+      <div class="d-flex flex-column align-items-center left">
+
+        <BrandLogo class="mb-3 " />
+
         <img
-          src="@/assets/images/left.svg"
+          src="@/assets/images/board_illustration.svg"
           alt="online todo list"
+          class="image-fluid"
           >
+
+      </div>
+
     </div>
 
     <div class="w-100 d-flex justify-content-center align-items-center">
+
       <router-view></router-view>
+
     </div>
 
   </div>
 </template>
 
+<script setup>
+import BrandLogo from '../components/BrandLogo.vue'
+</script>
+
 <style lang="scss" scoped>
-img {
+.left {
   width: 83.333%;
   max-width: 386px;
 }
