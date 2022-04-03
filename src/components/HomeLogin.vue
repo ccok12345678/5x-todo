0817723b972f90ebc1b4d87eb0c0da3ef16ac988
Karl Yang <ccok12345678@gmail.com>
2022-04-03 18:02:01 +0800

    added invalid message

--- src/components/HomeLogin.vue
+++ src/components/HomeLogin.vue
@@ -1,19 +1,33 @@
 <template>
   <div class="d-flex flex-column align-items-center">
-    <h4 class="fw-bold">最實用的線上待辦事項服務</h4>
+    <h4 class="fw-bold mb-4 align-self-start">最實用的線上待辦事項服務</h4>
 
-    <form class="form d-flex flex-column align-items-center"
+    <form class="form d-flex flex-column align-items-center w-100"
       @submit.prevent>
 
       <label for="email" class="align-self-start">Email</label>
-      <input type="email" name="email" id="email"
-        placeholder="請輸入Email">
+      <input
+        type="email"
+        name="email"
+        id="email"
+        placeholder="請輸入Email"
+        required>
+      <small class="invalid-msg text-danger align-self-start">
+        此欄位不可為空
+      </small>
 
       <label for="password" class="align-self-start">密碼</label>
-      <input type="password" name="password" id="password"
-        placeholder="請輸入密碼">
+      <input
+        type="password"
+        name="password"
+        id="password"
+        placeholder="請輸入密碼"
+        required>
+      <small class="invalid-msg text-danger align-self-start">
+        此欄位不可為空
+      </small>
 
-      <button type="submit" class="btn btn-dark mt-4">
+      <button type="submit" class="btn btn-dark mt-2">
         登入
       </button>
 
--- src/components/HomeRegister.vue
+++ src/components/HomeRegister.vue
@@ -1,27 +1,39 @@
 <template>
     <div class="d-flex flex-column align-items-center">
-    <h4 class="fw-bold align-self-start">註冊帳號</h4>
+    <h4 class="fw-bold align-self-start mb-4">註冊帳號</h4>
 
-    <form class="form d-flex flex-column align-items-center"
+    <form class="form d-flex flex-column align-items-center w-100"
       @submit.prevent>
 
       <label for="email" class="align-self-start">Email</label>
       <input type="email" name="email" id="email"
-        placeholder="請輸入Email">
+        placeholder="請輸入Email" required>
+      <small class="invalid-msg text-danger align-self-start">
+        此欄位不可為空
+      </small>
 
       <label for="nickname" class="align-self-start">您的暱稱</label>
       <input type="text" name="nickname" id="nickname"
-        placeholder="請輸入您的暱稱">
+        placeholder="請輸入您的暱稱" required>
+      <small class="invalid-msg text-danger align-self-start">
+        此欄位不可為空
+      </small>
 
       <label for="password" class="align-self-start">密碼</label>
       <input type="password" name="password" id="password"
-        placeholder="請輸入密碼">
+        placeholder="請輸入密碼" required>
+      <small class="invalid-msg text-danger align-self-start">
+        此欄位不可為空
+      </small>
 
       <label for="password" class="align-self-start">再次輸入密碼</label>
       <input type="password" name="password" id="password"
-        placeholder="請再次輸入輸入密碼">
+        placeholder="請再次輸入輸入密碼" required>
+      <small class="invalid-msg text-danger align-self-start">
+        此欄位不可為空
+      </small>
 
-      <button type="submit" class="btn btn-dark mt-4">
+      <button type="submit" class="btn btn-dark mt-2">
         註冊帳號
       </button>
 
