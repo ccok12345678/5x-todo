f05a666bb3dddba7d3a85d52ce19720e1d377bf6
Karl Yang <ccok12345678@gmail.com>
2022-04-04 23:28:38 +0800

    add todo number

--- src/components/todo/TodoFooter.vue
+++ src/components/todo/TodoFooter.vue
@@ -2,7 +2,7 @@
   <footer class="todo-footer d-flex justify-content-between align-items-center">
 
     <span>
-      5 個待完成項目
+      {{ unfinishedTodos.length }} 個待完成項目
     </span>
 
     <button type="button" class="btn p-0">
@@ -11,3 +11,11 @@
 
   </footer>
 </template>
+
+<script setup>
+import { storeToRefs } from 'pinia'
+import useTodoStore from '@/stores/todoStore'
+
+const todoStore = useTodoStore()
+const { unfinishedTodos } = storeToRefs(todoStore)
+</script>
